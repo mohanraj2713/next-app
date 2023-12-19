ca6c387689580ed5caff3c7e6afc025e48188298
Mohanraj Selvaraj <mohanraj.selvaraj@edufin.in>
2023-12-19 18:07:45 +0530

    will check automatically deployee

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
+        <h1>We are developers</h1>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
