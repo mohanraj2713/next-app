dd25c2281065305b38df297efca1275c4c7fea94
Mohanraj Selvaraj <mohanraj.selvaraj@edufin.in>
2023-12-19 16:49:55 +0530

    redux dev tools added

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,10 +1,11 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "./slice/UserSlice"
-
+import { composeWithDevTools } from "redux-devtools-extension";
 const store = configureStore({
-    reducer:{
-        userInfo:userReducer
+    reducer: {
+        userInfo: userReducer
     },
-})
+
+}, composeWithDevTools())
 
 export default store;
